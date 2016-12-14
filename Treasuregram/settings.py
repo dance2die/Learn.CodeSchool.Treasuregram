5d8b5851a1306c9cb0def1d77d0f8628263fe500
dance2die <dontbothermewithspam@gmail.com>
2016-12-14 06:52:53 -0500

    correctly connects to sql server

--- Treasuregram/settings.py
+++ Treasuregram/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'dmfx8d!edw2t(4c$8hc(h17xn7pak-9ehah5ot4+9z*&h-uqr='
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,17 +69,28 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Treasuregram.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'INITIAL_CATALOG': 'Test',
+        'ENGINE': 'sqlserver_ado',
+        'HOST': 'GODDESS\\SQL2014',
+        'NAME': 'TEST',
+        'USER': '',
+        'PASSWORD': '',
+        # http://stackoverflow.com/a/32863983/4035
+        'OPTIONS': {
+            'provider': 'SQLOLEDB',
+            'use_legacy_date_fields': 'True'
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -101,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -114,7 +122,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
